e3a056e3b0004a062a8d7a5c3d440dfba3bc3198
Pedro-dev-083 <pedroazevedo83@hotmail.com>
2023-12-27 12:27:54 -0300

    update novo

---
+++ scripts/pullRequestPreview.js
@@ -0,0 +1,10 @@
+import { execSync } from "child_process";
+
+console.log("[DEPLOY_PREVIEW]: START");
+const command = "yarn deploy";
+const output = execSync(command, { encoding: "utf8" });
+console.log(output);
+console.log("[DEPLOY_PREVIEW]: END");
+
+// console.log("[GITHUB_COMMENT]: START");
+// console.log("[GITHUB_COMMENT]: END");
